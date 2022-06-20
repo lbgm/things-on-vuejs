4898953c833a56b9b0fe8428ada04cbdfd16634f
Balthazar DOSSOU <92580505+lbgm@users.noreply.github.com>
2022-06-20 20:35:14 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import router from "./router";
 import store from "./store";
 
 import OtpInput from "@/components/otp-input.vue";
-import RangeInput from "@/components/range-input.vue";
 import OtpInputV2 from "@/components/otp-input-vue2.vue";
 
 const app = createApp(App);
@@ -13,7 +12,6 @@ app.use(store);
 app.use(router);
 
 app.component("OtpInput", OtpInput);
-app.component("RangeInput", RangeInput);
 app.component("OtpInputV2", OtpInputV2);
 
 app.mount("#app");
